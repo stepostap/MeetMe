6fbfc6da74277bcf45deb54f2d71cee02d44e57d
stepostap <stepan@ostapenko.net>
2022-02-24 16:12:43 +0300

    init

---
+++ MeetMe/MeetMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeetMe
+//
+//  Created by Stepan Ostapenko on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
